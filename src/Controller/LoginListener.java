3f3635536bc9dcf5cc1bda58d8848a4d870f91e3
MEUS DOCUMENTOS <MEUS DOCUMENTOS@172.16.26.144>
2016-09-23 19:11:09 -0300

    ALterações

--- src/Controller/LoginListener.java
+++ src/Controller/LoginListener.java
@@ -1,27 +1,27 @@
-
 package Controller;
-
-import Janelas.Estacionamento;
-import Janelas.LoginFunc;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.JOptionPane;
-
-public class LoginListener implements ActionListener{
-        private LoginFunc frame;
-
-    public LoginListener(LoginFunc frame) {
-        this.frame = frame;
-    }
-
-    @Override
-        public void actionPerformed(ActionEvent e) {
-        if ("login".equals(e.getActionCommand())) {
-            JOptionPane.showMessageDialog(null, "Desenvolvido por José Vitor");
-            //if(frame.getUsuario().equals("1")){
-            //    Estacionamento est = new Estacionamento();
-            }
-        } 
-        
-    }
-    
+ 
+ import Janelas.Estacionamento;
+ import Janelas.LoginFunc;
+ import java.awt.event.ActionEvent;
+ import java.awt.event.ActionListener;
+ import javax.swing.JOptionPane;
+ 
+ public class LoginListener implements ActionListener {
+         
+       private  LoginFunc frame;
+         
+       public LoginListener (LoginFunc frame){
+           this.frame = frame;
+       }
+       
+       public void actionPerformed(ActionEvent e){
+           if("login".equals(e.getActionCommand())){
+               JOptionPane.showMessageDialog(null, "Bem vindo!");
+             new Estacionamento().setVisible(true);
+           }
+           else {
+                 JOptionPane.showMessageDialog(null, "Login inválido");
+             }
+       }
+     
+ }
