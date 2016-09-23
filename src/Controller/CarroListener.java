1f431a4b4cae5fe5a8a5242eab805f812663adda
MEUS DOCUMENTOS <MEUS DOCUMENTOS@172.16.26.144>
2016-09-23 19:43:29 -0300

    ALterações

--- src/Controller/CarroListener.java
+++ src/Controller/CarroListener.java
@@ -1,24 +1,24 @@
-
 package Controller;
-
 
 import Janelas.CarroFrame;
 import java.awt.Event;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Locale;
+import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
 
 public class CarroListener implements ActionListener{
     
         private  CarroFrame carro;
-                
+        
       public CarroListener (CarroFrame frame){
           this.carro = frame;
       }
       
       public void actionPerformed(ActionEvent e){
+        
           
         if ("sair".equals(e.getActionCommand())){
             carro.dispose();
